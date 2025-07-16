853a2c6b2e2cc7ddb28d05a5ed6cf1f1acfa0d75
talraj bhatia <talrajsingh13@gmail.com>
2025-07-16 12:53:49 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from '../components/hero'
+import Feature from '../components/Feature'
+import Offers from '../components/offers'
+
+const Home = () => {
+  return (
+    <div>
+      <>
+      <Hero/>
+      <Feature/>
+      <Offers/>
+      </>
+    </div>
+  )
+}
+
+export default Home
